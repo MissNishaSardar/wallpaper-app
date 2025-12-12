e8ac845b11b8e9585c69ce84d55991601c2a48ad
Nisha Sardar <missnishasardar@gmail.com>
2025-12-12 17:21:50 +0530

    Bugs fixing, dependencies bump

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -10,7 +10,7 @@ const StudioLayout = ({ children }: Readonly<StudioLayoutProps>) => {
 		<>
 			<Header />
 
-			<main className="mx-auto max-w-7xl px-6 py-3">{children}</main>
+			<main className="mx-auto max-w-7xl px-6 pt-16 pb-2">{children}</main>
 		</>
 	);
 };
